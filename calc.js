eed7fb212d07312961c5547c4e9b12d605971653
sandeep singh pal rajput <139905756+sandeepraj100sp@users.noreply.github.com>
2023-07-19 00:04:54 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,19 @@
+let string = "";
+let buttons = document.querySelectorAll('.button'); // query selector select all the elements of button class 
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string); //If the argument is an expression, eval() evaluates the expression
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){    // e.target gives jo button click hua he
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
